c438b4f1a9034ccbde7aca7dc04e649f89fac0da
Garon <garoncdavis@gmail.com>
2017-05-18 09:33:25 -0400

    reduxifying the login in screen

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+
+import reducers from './reducers';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyA65HfUk-e1O7YNuzPGbXWr4FonPNCUhwQ',
+      authDomain: 'manager-ab2d8.firebaseapp.com',
+      databaseURL: 'https://manager-ab2d8.firebaseio.com',
+      projectId: 'manager-ab2d8',
+      storageBucket: 'manager-ab2d8.appspot.com',
+      messagingSenderId: '137464946433'
+    };
+firebase.initializeApp(config);
+  }
   render() {
     return (
-      <Provider>
+      <Provider store={createStore(reducers)}>
         <View>
-          <Text>
-            Hello!
-          </Text>
+          <LoginForm />
         </View>
       </Provider>
     )
